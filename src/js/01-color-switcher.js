1eb4e53c987ff0de70cbc0b9033461ec1650bacf
Andrii Gargat <andreysax1312@gmail.com>
2022-10-31 16:51:40 +0200

    color-swicher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,52 @@
+let getEl = selector => document.querySelector(selector); 
+const startBtnRef = getEl('[data-start]');
+const stopBtnRef = getEl('[data-stop]');
+const bodyRef = getEl('body');
 
+const delay = 1000;
+let intervalId = null;
+stopBtnRef.setAttribute('disabled', true);
+
+function startColorSwich() { 
+    startBtnRef.setAttribute('disabled', true);
+    stopBtnRef.removeAttribute('disabled');
+    intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+    }, delay);
+};
+
+function stopColorSwich() {
+    stopBtnRef.setAttribute('disabled', true);
+    startBtnRef.removeAttribute('disabled');
+    clearInterval(intervalId);
+};
+
+startBtnRef.addEventListener('click', () => startColorSwich()); 
+stopBtnRef.addEventListener('click', () => stopColorSwich());
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+// ==============================================================================
+//Варіант 2 - написання через клас
+
+// class Rainbow {
+//     start() { 
+//         startBtnRef.setAttribute('disabled', true);
+//         stopBtnRef.removeAttribute('disabled');
+//         this.intervalId = setInterval(() => {
+//         let randomColor = getRandomHexColor();
+//         bodyRef.style.backgroundColor = randomColor;
+//         }, delay);
+//     };
+
+//     stop() {       
+//         stopBtnRef.setAttribute('disabled', true);
+//         startBtnRef.removeAttribute('disabled');
+//         clearInterval(this.intervalId)
+//     };
+// };
+
+// const rainbow = new Rainbow();
+// startBtnRef.addEventListener('click', () => rainbow.start()); 
+// stopBtnRef.addEventListener('click', () => rainbow.stop());
